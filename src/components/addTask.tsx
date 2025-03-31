c751dee504ef6432b7cc8bd78509cb17f6ad1462
santanaIgot <santanaigoor01@gmail.com>
2025-03-31 18:00:35 -0300

    [feat] - taskItem

--- src/components/addTask.tsx
+++ src/components/addTask.tsx
@@ -2,6 +2,8 @@ import { useState, ChangeEvent } from "react";
 import styles from "./addTask.module.css";
 import { TaskItem } from "./taskItem";
 
+import img from "../image/Clipboard.png";
+
 interface TaskType {
   id: number;
   content: string;
@@ -31,29 +33,40 @@ export function AddTask() {
 
   return (
     <>
-      <div className={styles.newTask}>
-        <textarea
-          className={styles.input}
-          placeholder="ADICIONE UMA NOVA TAREFA"
-          value={task}
-          onChange={handleNewAddTask}
-        />
-        <button onClick={handleAddTask} className={styles.button}>
-          Criar nova tarefa
-        </button>
-      </div>
-
-      <div className={styles.task}>
-        <div className={styles.taskCount}>
-          <p className={styles.created}>
-            Tarefas criadas <span>{countTask}</span>
-          </p>
-          <p className={styles.created}>Concluídas</p>
+      <div className={styles.taskContainer}>
+        <div className={styles.newTask}>
+          <textarea
+            className={styles.input}
+            placeholder="ADICIONE UMA NOVA TAREFA"
+            value={task}
+            onChange={handleNewAddTask}
+          />
+          <button onClick={handleAddTask} className={styles.button}>
+            Criar nova tarefa
+          </button>
         </div>
 
-        {tasks.map((task) => (
-          <TaskItem key={task.id} content={task.content} />
-        ))}
+        <div className={styles.task}>
+          <div className={styles.border}>
+            <div className={styles.taskCount}>
+              <p className={styles.created}>
+                Tarefas criadas <span>{countTask}</span>
+              </p>
+              <p className={styles.created}>Concluídas</p>
+            </div>
+          </div>
+          <div className={styles.taskEmpty}>
+            <img src={img} alt="" className={styles.img} />
+            <p className={styles.p}>Você ainda não tem tarefas cadastradas.</p>
+            <p className={styles.p}>
+              Crie tarefas e organize seus intens a fazer
+            </p>
+          </div>
+
+          {tasks.map((task) => (
+            <TaskItem key={task.id} content={task.content} />
+          ))}
+        </div>
       </div>
     </>
   );
